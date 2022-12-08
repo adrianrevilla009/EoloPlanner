6c773ee81bfee66919a28d70ddfdf6eaa0ba45c2
adrianrevilla009 <adrianrg1996@gmail.com>
2022-12-08 10:39:50 +0100

    refactor

---
+++ topoService/src/main/java/masterCloudApps/comunications/topoService/TopoServiceApplication.java
@@ -0,0 +1,13 @@
+package masterCloudApps.comunications.topoService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopoServiceApplication.class, args);
+	}
+
+}
---
+++ topoService/src/test/java/masterCloudApps/comunications/topoService/TopoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package masterCloudApps.comunications.topoService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
